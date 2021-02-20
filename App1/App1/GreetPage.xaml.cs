874254966618bc3e0ebf8014481833a3493601d3
Shaq <shaqdulove@hotmail.com>
2021-02-19 20:33:40 -0400

    first commit

---
+++ App1/App1/GreetPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GreetPage : ContentPage
+	{
+		public GreetPage ()
+		{
+			InitializeComponent ();
+
+            mySlider.Value = 0.1;
+
+            switch (Device.RuntimePlatform)
+            {
+                case ("iOS"):
+                    Padding = new Thickness(0, 20, 0, 0);
+                    break;
+                default:
+                    break;
+            }
+		}
+
+        private void Handle_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Title", "Hello World", "OK");
+        }
+
+    }
+}
---
+++ App1/App1/Quotes.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Quotes : ContentPage
+	{
+        public string[] myQuotes = new []{ "Testing","Consentwise","Xamarin"};
+        public int iterationState = 0;
+		public Quotes ()
+		{
+			InitializeComponent ();
+            quotes.Text = myQuotes[0];
+            fontSlider.Value = 17;
+		}
+
+        private void Handle_Quotes(object sender, EventArgs e)
+        {
+            if (iterationState < 2) { iterationState++; }
+            else { iterationState = 0; }
+            quotes.Text = myQuotes[iterationState];
+        }
+	}
+}
